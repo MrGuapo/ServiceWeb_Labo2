b8ab00c8340fbbdb41fcbd24dbe59537c8fb84db
MrGuapo <108371628+MrGuapo@users.noreply.github.com>
2024-02-23 15:59:43 -0500

    Code first version of DeleteCardByNumber

--- frontend/src/components/features/DeleteCardByNumber.js
+++ frontend/src/components/features/DeleteCardByNumber.js
@@ -1,5 +1,9 @@
 import {useState} from "react";
 import axios from "axios";
+import {Button, Container, FormControl, Input, InputLabel, Typography} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {Link} from "react-router-dom";
+import NavBar from "../common/NavBar";
 
 const DeleteCardByNumber = () => {
     const [cardNumber, setCardNumber] = useState("");
@@ -20,14 +24,33 @@ const DeleteCardByNumber = () => {
     };
 
     return (
-        <div className="DeleteCardByNumber">
-            <input type="text" value={cardNumber} onChange={handleInputChange}/>
-            {isDeleted ? (
-                <p>Card with number {cardNumber} has been successfully deleted.</p>
-            ) : (
-                <button onClick={deleteCard}>Delete Card</button>
-            )}
-            {error && <p>Error: {error}</p>}
+        <div className="DeleteCardByNumber" style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+            <NavBar text="Supprimer une carte par son numero"/>
+            <Container style={{
+                flex: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <FormControl variant="standard" style={{width: "300px", textAlign: "center"}}>
+                    <InputLabel htmlFor="cardNumber">Numero de carte</InputLabel>
+                    <Input type="text" value={cardNumber} onChange={handleInputChange}/>
+                    {isDeleted ? (
+                        <Typography sx={{color: "green"}}>La carte avec le numéro {cardNumber} a été supprimée avec succès!</Typography>
+                    ) : (
+                        <Button onClick={deleteCard} variant="contained" startIcon={<DeleteIcon/>}
+                                style={{marginTop: "5px"}}>
+                            Supprimer carte
+                        </Button>
+                    )}
+                </FormControl>
+            </Container>
+            <div style={{alignSelf: "flex-end", marginRight: "10px", marginBottom: "10px"}}>
+                <Link to="/">
+                    <Button variant="contained">Retour a l'accueil</Button>
+                </Link>
+            </div>
         </div>
     );
 };
